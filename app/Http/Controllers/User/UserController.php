c2f8db36d9fa58abfbfa002c595fd1166e8979f0
Isaac Oduh <isaacoduh@pc-200-57.customer.ask4.lan>
2023-06-18 21:39:45 +0100

    create user order page, view all details of order

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -24,6 +26,17 @@ class UserController extends Controller
 
     public function UserOrderPage()
     {
-        return view('frontend.user.user_order_page');
+        $id = Auth::user()->id;
+        $orders = Order::where('user_id', $id)->orderBy('id', 'DESC')->get();
+        return view('frontend.user.user_order_page',compact('orders'));
     }
+
+    public function UserOrderDetails($order_id)
+    {
+        $order = Order::with('state','area','user')->where('id',$order_id)->where('user_id', Auth::id())->first();
+        $orderItem = OrderItem::with('product')->where('order_id', $order_id)->orderBy('id','DESC')->get();
+
+        return view('frontend.order.order_details',compact('order','orderItem'));
+    }
+
 }
